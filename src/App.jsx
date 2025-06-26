2672923fa04fb43cddee985aefbea168128ee4c6
flook25 <flook11223344@gmail.com>
2025-06-26 17:31:00 +0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import Home from "./components/Home"
 import About from "./components/About"
 import Blogs from "./components/Blogs"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-// About -> about
-// blogs -> blogs
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Link to="/">
+        <h3>Blog Application</h3>
+      </Link>
+      <Link to="/">Fist Page</Link>
+      <Link to="/blogs">All of the blogs</Link>
+
+      <Link to="/about">About</Link>
+    </nav>
+  );
+}
