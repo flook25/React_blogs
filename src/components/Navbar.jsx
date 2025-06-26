53540c60c2d934d0d398e83391e75e162faf0f65
flook25 <flook11223344@gmail.com>
2025-06-26 18:28:33 +0700

    fix css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
+import React from 'react';
 import { Link } from "react-router-dom";
-import './Navbar.css'
+import './Navbar.css';
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link to="/">
-        <h3>Blog Application</h3>
+    <nav className="navbar-container"> {/* Added a class for specific styling */}
+      {/* Blog Application (left) */}
+      <Link to="/" className="navbar-brand-link"> {/* Added class for specific styling */}
+        <h3 className="logo">Blog Application</h3>
       </Link>
-      <Link to="/">Fist Page</Link>
-      <Link to="/blogs">All of the blogs</Link>
 
-      <Link to="/about">About</Link>
+      {/* Navigation links (right) */}
+      <div className="nav-links-group"> {/* New div to group right links */}
+        <Link to="/" className="nav-link">Fist Page</Link>
+        <Link to="/blogs" className="nav-link">All of the blogs</Link>
+        <Link to="/about" className="nav-link">About</Link>
+      </div>
     </nav>
   );
 }
