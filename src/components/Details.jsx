eaca73e5b7564471d9501d6e0d2096d8d7f01905
flook25 <flook11223344@gmail.com>
2025-06-26 21:22:33 +0700

    add Details.css

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import blogs from "../data/blogs"
+import './Details.css'
 
 export default function Details() {
 
