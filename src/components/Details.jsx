5297a2adfe71eef15056a7e4b56886478eef3ee8
flook25 <flook11223344@gmail.com>
2025-06-26 19:54:55 +0700

    adding show content

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,18 +1,30 @@
 import { useParams } from "react-router-dom"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import blogs from "../data/blogs"
 
 export default function Details() {
 
     const {id} = useParams()
+    const [title, setTitle] = useState('');
+    const [image, setImage] = useState('');
+    const [content, setContent] = useState('');
+    const [author, setAuthor] = useState('');
     useEffect(() => {
             //get data article
             const result = blogs.find((item)=>item.id === parseInt(id))
-            console.log(result)
+            setTitle(result.title)
+            setImage(result.image_url)
+            setContent(result.content)
+            setAuthor(result.author)
     }, [])
     return (
-        <>
-        <h2 >Detail of Article: {id}</h2>
-        </>
+       <div className="container">
+        <h2 className="title">Article : {title}</h2>
+        <img src={image} alt={title} />
+        <div className="blog-detail">
+            <strong>Author : {author}</strong>
+            <p>{content}</p>
+        </div>
+       </div>
     )
 }
